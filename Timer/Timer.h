b66f3b89212f1b59f3a4f21f4c770418113164c7
matt <matthieu.magnon@gadz.org>
2019-12-31 12:31:41 +0100

    Timer: add timer library

---
+++ Timer/Timer.h
@@ -0,0 +1,30 @@
+/*
+  Timer.h - April 2016
+  Copyright (c) 2016.  All rights reserved.
+  Auteur : Jérémy LONGER
+  Company : Kuehne + Nagel
+  Modifiation : April 2016
+  Description : 
+ */
+#ifndef DEF_TIMER
+#define DEF_TIMER
+
+class Timer
+{
+  public:
+    Timer(unsigned long T);
+    void setDelay(unsigned long T);
+    bool delay(unsigned long M, unsigned long T);
+    bool delay(unsigned long M);
+    float getG_Dt() const;
+    void start(unsigned long M);
+
+  private:
+    unsigned long m_timer;
+    unsigned long m_timer_old;
+    unsigned long m_T;
+    float m_G_Dt;
+  
+};
+
+#endif
